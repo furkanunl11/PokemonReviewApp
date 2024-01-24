5a2d8d8bf8e4f0fcefb6197dab34a715d62c3042
Furkan <91071694+furkanunl11@users.noreply.github.com>
2024-01-24 23:51:15 +0300

    up

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ICountryRepository _countryRepository;
+
+        public CountryController(ICountryRepository countryRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _countryRepository = countryRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Country>))]
+
+        public IActionResult GetCountries()
+        {
+            var countries = _mapper.Map<List<CountryDto>>(_countryRepository.GetCountries());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(countries);
+        }
+
+        [HttpGet("{countryId}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+
+        [ProducesResponseType(400)]
+
+        public IActionResult GetCountry(int countryId)
+        {
+            if (!_countryRepository.CountryExist(countryId))
+            {
+                return NotFound();
+
+            }
+
+            var country = _mapper.Map<CountryDto>(_countryRepository.GetCountry(countryId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+
+            }
+
+            return Ok(country);
+
+        }
+
+        [HttpGet("/owners/{ownerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(Country))]
+
+        public IActionResult GetCountryOfAnOwner(int ownerId)
+        {
+            var country = _mapper.Map<CountryDto>(
+                _countryRepository.GetCountryByOwner(ownerId));
+
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(country);
+        }
+    }
+}
---
+++ Interfaces/ICountryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository 
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+
+        Country GetCountryByOwner(int ownerId);
+        ICollection<Owner> GetOwnersFromACountry(int countryId);
+
+        bool CountryExist(int id);
+    }
+}
